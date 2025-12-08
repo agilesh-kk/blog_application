efda7ba94480037b72c9a99f7c4def336e4256b2
agilesh-kk <23cseb34.agileshkk@gmail.com>
2025-12-08 19:41:23 +0530

    fixed sign up bug

--- lib/features/auth/presentation/pages/signup_page.dart
+++ lib/features/auth/presentation/pages/signup_page.dart
@@ -46,14 +46,14 @@ class _SignupPageState extends State<SignupPage> {
             if(state is AuthFailure){
               showSnackbar(context, state.message);
             }
+            //if sigining up the user is successful then it routes to blogpage.
+            else if(state is AuthSuccess){
+              context.goNamed(BlogPage.pageName);
+            }
           },
           builder: (context, state){
             if(state is AuthLoading){
               return const Loader();
-            }
-            //if sigining up the user is successful then it routes to blogpage.
-            else if(state is AuthSuccess){
-              context.goNamed(BlogPage.pageName);
             }
 
             return Form(
